c647897f4190317e56e5cffed7d24eb4bb8a0ae8
shaktigurung <mr.shakti.gurung@gmail.com>
2018-12-05 17:48:11 +1100

    Mongoose added and connected

--- myApp/app.js
+++ myApp/app.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodemyApp');
+let db = mongoose.connection;
+//Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+//Check for DB errors
+db.on('error', function(err){
+    console.log(err);
+});
+
 //Init app
 const app = express();
 const port = 3000;
 
+//Bring in models
+let Article = require('./models/article');
 //Load View Engine
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'pug');
 
 //Home route
 app.get('/', (req, res) => {
-    res.render('index',{
-        title: 'Articles'
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err);
+        } else {
+        res.render('index',{
+            title: 'Articles',
+            articles: articles
+        });
+        }
     });
+   
 });
 
 //Add Route
