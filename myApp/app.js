34ccb971dfb3d0ad830453c0bfea5deb69b14d0f
shaktigurung <mr.shakti.gurung@gmail.com>
2018-12-05 17:16:40 +1100

    Express nodemon n forever setup

--- myApp/app.js
+++ myApp/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const path = require('path');
+//Init app
 const app = express();
 const port = 3000;
 
+//Load View Engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'pug');
+
+//Home route
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index',{
+        title: 'Articles'
+    });
 });
 
+//Add Route
+app.get('/articles/add', (req, res)=>{
+    res.render('add_article',{
+        title: 'Add Article'
+    });
+})
+
+
+//Start Server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}!`);
 });
