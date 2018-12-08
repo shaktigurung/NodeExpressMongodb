e776bbd22d83f33029e6d1a6090b2337184eea06
shaktigurung <mr.shakti.gurung@gmail.com>
2018-12-08 19:01:45 +1100

    handlebars

--- EmployeeList/app.js
+++ EmployeeList/app.js
@@ -1,10 +1,18 @@
 require('./models/db');
 
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const employeeController = require('./controllers/employeeController');
 
 const app = express();
+
+app.set('views', path.join(--dirname, './../views/'));
+app.engine('hbs', exphbs({
+    extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + './../views/layouts/'
+}));
+app.set('view engine', 'hbs');
 
 app.listen(3000, ()=>{
     console.log("Express Server started at port: 3000");
