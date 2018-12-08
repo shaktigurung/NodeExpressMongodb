ff0c3cd8029fa1bed2c34ded03a43abc6530d576
shaktigurung <mr.shakti.gurung@gmail.com>
2018-12-08 17:49:52 +1100

    EmployeeList app

--- ProductApp/server.js
+++ ProductApp/server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+//const bodyParser = require('body-parser');
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/productapp", { useNewUrlParser: true });
+// //DB config
+// const db = require("./config/keys").mongoURI;
+
+// //Connect to Mongo
+// mongoose.connect(db)
+//     .then(()=> console.log("MongoDB connected..."))
+//     .catch(err => console.log(err));
+
+const db = "mongodb://127.0.0.1/productapp";
+mongoose.connect(db, ()=>{
+    console.log("Database connected..");
+});
 mongoose.Promise = global.Promise;
 mongoose.connection.on("error", console.log);
 
 const port = 3000;
 
-// app.get("/", (req, res)=>{
-//     res.send("Hello World again wow. Good to see you running!");
-// });
-
+app.get("/", (req, res)=>{
+    //let product = db.myProduct.find();
+    res.send("Hello World again wow. Good to see you running!");
+  
+});
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
