9f9d1cb650a178ad99f79380fa78f5bfca57a194
shaktigurung <mr.shakti.gurung@gmail.com>
2018-12-09 14:25:39 +1100

    Server Setup for Development done

--- BooksRUs/app.js
+++ BooksRUs/app.js
@@ -6,6 +6,21 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+//Mongoose Database connection
+mongoose.connect("mongodb://localhost/books_r_us", { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+mongoose.connection.on("Error", err => console.log(err));
+
+//Setup Handle-bars
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//Body-parser as middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//Server
 app.listen(port, ()=>{
     console.log(`Server is runnning on port ${port}`);
 })
